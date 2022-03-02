d2e91d9eb91c170f0c4e704b894cf23494381916
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-02 14:15:03 +0800

    add register page

---
+++ register.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Mart Anthony Salazar</title>
+</head>
+
+<body>
+    <div id="root">
+        <form @submit='submitForm'>
+            <span id="icon">
+                <i class="fa fa-user-circle" aria-hidden="true"></i>
+            </span>
+            <div class='field'>
+                <span class="iconify" data-icon="ant-design:user-outlined"></span>
+                <input type='text' placeholder='Fullname' />
+            </div>
+
+            <div class='field'>
+                <span class="iconify" data-icon="ant-design:user-outlined"></span>
+                <input type='text' placeholder='Username' />
+            </div>
+
+            <div class='field'>
+                <span class="iconify" data-icon="feather:lock"></span>
+                <input type='password' placeholder='Password' />
+
+            </div>
+            <button>Signup</button>
+            <h4>Already have an account? <span @click='login'>Login</span>
+                <h4>
+        </form>
+    </div>
+    <img id='right' src="./assets/flat.svg" alt="asd">
+    <img id='circle-1' src="./assets/circle1.svg" alt="">
+    <img id='circle-2' src="./assets/circle2.svg" alt="">
+    <img id='circle-2' src="./assets/circle3.svg" alt="">
+
+
+
+    <script src="https://code.iconify.design/2/2.0.3/iconify.min.js"></script>
+</body>
+
+</html>
